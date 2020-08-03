9a86488b04b3f262bb04bd51383b3ece623ef6e9
Dan- <misternutel@gmail.com>
2020-08-02 23:37:22 -0500

    code optimization

--- riordinato/Riordinato.py
+++ riordinato/Riordinato.py
@@ -1,26 +1,34 @@
 from shutil import move
-from os import chdir, listdir
+from os import chdir, listdir, scandir
 
 
 class Organize():
-
     def __init__(self, prefixes, dirLocation):
         """
         prefixes = [(<prefix>, <Destination>), 
                     (<prefix>, <Destination>),...]
+
+        dirLocation = /home/user/...
         """
         self.prefixes = prefixes
         self.dirLocation = dirLocation
 
+    def __scan_files(self):
+        with scandir(self.dirLocation) as files:
+            files = [afile.name for afile in files if files.is_file()]
+
+        return files
+
     def organize_specific_files(self, prefix):
         """move files with a specific prefix"""
         chdir(self.dirLocation)
-        files = listdir('./')
+
+        files = self.__scan_files
 
         for i in range(len(self.prefixes)):
             if prefix == self.prefixes[i][0]:
                 for afile in files:
-                    if prefix in afile and '.' in afile:
+                    if prefix in afile:
                         move(afile, self.prefixes[i][1])
                         print(afile, "moved")
                     else:
@@ -33,6 +41,9 @@ class Organize():
 
         for afile in files:
             for i in range(len(self.prefixes)):
-                if self.prefixes[i][0] in afile and '.' in afile:
+                if self.prefixes[i][0] in afile:
                     move(afile, self.prefixes[i][1])
                     break
+
+    def __str__(self):
+        return self.prefixes
--- riordinato/__init__.py
+++ riordinato/__init__.py
@@ -1,6 +1,6 @@
 from riordinato import Riordinato
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.3'
 
 __all__ = ['Riordinato']
