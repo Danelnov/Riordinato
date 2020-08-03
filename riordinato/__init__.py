b4cb0a68966dc2d0a96f5eaf11c106dfe4a1c0d5
Dan- <misternutel@gmail.com>
2020-08-03 09:25:24 -0500

    code optimization

--- riordinato/__init__.py
+++ riordinato/__init__.py
@@ -1,7 +1,9 @@
 from shutil import move
-from os import chdir, listdir, scandir
+from os import chdir, listdir, scandir, is_file
+import re
 
-__version__ = '0.1.5'
+__version__ = '0.2.1'
+
 
 class Organize():
     def __init__(self, prefixes, dirLocation):
@@ -14,38 +16,41 @@ class Organize():
         self.prefixes = prefixes
         self.dirLocation = dirLocation
 
-    def __scan_files(self):
+    def __get_files(self):
+        """get only files from directory"""
         with scandir(self.dirLocation) as files:
             files = [afile.name for afile in files if files.is_file()]
 
+        return files
+    
+    def __get_files_wp(self, prefix, files):
+        """get the files that have the prefix through a regular expression"""
+        regex = "^{}(...|\w+)\B.+".format(prefix)
+        files = [afile for afile in files if re.finditer(regex, afile, re.IGNORECASE)]
         return files
 
     def organize_specific_files(self, prefix):
         """move files with a specific prefix"""
         chdir(self.dirLocation)
 
-        files = self.__scan_files
+        files = self.__get_files()
+        files = self.__get_files_wp(prefix, files)
 
-        for i in range(len(self.prefixes)):
-            if prefix == self.prefixes[i][0]:
+        for prefix in self.prefixes:
+            if prefix == prefix[0]:
                 for afile in files:
-                    if prefix in afile:
-                        move(afile, self.prefixes[i][1])
-                        print(afile, "moved")
-                    else:
-                        print(afile, "It does not move")
+                    move(afile, prefix[1])
                 break
 
     def organize_all(self):
         chdir(self.dirLocation)
-        files = listdir('./')
 
-        for afile in files:
-            for i in range(len(self.prefixes)):
-                if self.prefixes[i][0] in afile:
-                    move(afile, self.prefixes[i][1])
-                    break
+        files = self.__get_files()
+
+        for prefix in self.prefixes:
+            pfiles = self.__get_files_wp(prefix[0], files)
+            for afile in pfiles:
+                move(afile, prefix[1])
 
     def __str__(self):
         return self.prefixes
-
