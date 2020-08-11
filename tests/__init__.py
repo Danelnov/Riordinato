503b1b2b25aefe38abc2f2e9f63c01293de58209
Dan- <misternutel@gmail.com>
2020-08-10 23:39:09 -0500

    add test

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+from riordinato import *
--- tests/test_riordinato.py
+++ tests/test_riordinato.py
@@ -1,5 +1,17 @@
-from riordinato import __version__
+from riordinato.files import Organize
+import unittest
+import os
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestFiles(unittest.TestCase):
+    
+    def get_files(self):
+        dir = "/home/daniel/Escritorio/test"
+        with os.scandir(dir) as files:
+            files = [afile.name for afile in files if afile.is_file()]
+        
+        order = Organize("", dir).get_files()
+        self.assertEqual(order, files)
+        
+if __name__ == "__main__":
+    unittest.main()
