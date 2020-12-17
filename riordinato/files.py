dc62533d6ff0bead4cbc1e5c3b6f784ea0d6d398
Dan- <misternutel@gmail.com>
2020-12-17 01:29:00 -0500

    new test and the moveFiles method was modified

--- riordinato/files.py
+++ riordinato/files.py
@@ -2,58 +2,110 @@ from shutil import move
 import os
 import re
 
+# TODO: use pathlib
 
 class Organize():
+    """
+    Create prefixes and use them to sort files in different folders.
+
+    Attributes
+    ----------
+    prefixes: list
+        list containing prefixes name and destination place.
+    path: str
+        The folder location where the files to be moved are located.
+    files: list
+        The files found in the path.
+
+    Methods
+    -------
+    moveSpecificFiles(prefix, destination)
+        Move files with a specific prefix.
+    MoveFiles(specific=None)
+        Move all files that are in the path.
+    getFiles()
+        Get the files that are in the path attribute.
+    getFilesWP(prefix)
+        Get files with prefixes.
+
+    Examples
+    --------
+    >>> prefixes = [('python', '/home/user/documents/python')]
+    >>> path = "/home/user/desktop/python"
+    >>> a = Organize(prefixes, path)
+
+    """
+
     def __init__(self, prefixes: list, path: str):
         """
-        prefixes = [(<prefix>, <Destination>), 
-                    (<prefix>, <Destination>),...]
-
-        path = '/home/user/...'
+        Parameters
+        ----------
+        prefixes: list
+            list containing prefixes name and destination place.
+        path: str
+            The folder location where the files to be moved are located.
+        files: list
+            The files found in the path.
         """
         self.prefixes = prefixes
         self.path = path
         self.files = self.getFiles()    # get files from path
 
-    def organize_specific_files(self, prefix: str):
-        """move files with a specific prefix"""
+    def moveSpecificFiles(self, prefix: str, destination: str):
+        """Move files with a specific prefix.
+
+        Parameters
+        ----------
+        prefix: str
+            The prefix that the files must contain.
+        destination: str
+            The directory where files containing the prefix will be moved.
+        """
 
         # Place the program within the address given by the path attribute
         os.chdir(self.path)
 
-        # Get files that have the prefix
+        # Generates a list of files containing the prefix
         files = self.getFilesWP(prefix)
 
-        # Move files to path
+        # Move files to destination
         for aprefix in self.prefixes:
-            if aprefix[0] == prefix: # First check if prefix are in self.prefixes
-                for afile in files:
-                    move(afile, aprefix[1])
+            if aprefix[0] == prefix:  # Check if prefix are in self.prefixes
+                for file in files:
+                    move(file, destination)
                 break
 
-    def MoveFiles(self):
-        """move all files that are in the path"""
-        
-        # Place the program within the address given by the path attribute
-        os.chdir(self.path)
+    def moveFiles(self, specific: str = None):
+        """Move all files that are in the path
+
+        Parameters
+        ----------
+        specific: str, optional
+            Move only files containing this prefix (default is None)
+        """
 
         # Move each file
         for prefix in self.prefixes:
-            # Generates a list of files containing the prefix
-            pfiles = self.getFilesWP(prefix[0]) 
-            # If the list is not empty start moving them
-            if pfiles:
-                for afile in pfiles:
-                    move(afile, prefix[1])
+            if specific == None:
+                destination = prefix[1]
+                self.moveSpecificFiles(prefix[0], destination)
+            else:
+                if prefix[0] == specific:  # Check if specific are in self.prefixes
+                    destination = prefix[1]
+                    self.moveSpecificFiles(prefix[0], destination)
+                    break
 
     def getFiles(self) -> list:
         """Get the files that are in the path attribute.
 
-        Get all the files that are on the path, excluding the folders 
-        and returning only the files.
+        Return
+        ------
+        list        
+            All the files that are on the path, excluding the folders.
         """
         path = self.path
 
+        # Get the files
         with os.scandir(path) as files:
             files = [afile.name for afile in files if afile.is_file()]
 
@@ -61,14 +113,18 @@ class Organize():
 
     def getFilesWP(self, prefix: str) -> list:
         """Get files with prefixes
-        
+
         get the files that have the prefix through a regular expression
 
-        Use the filter function to be able to find within 
-        the list the files that contain the assigned prefix.
+        Parameters
+        ----------
+        prefix: str
+            Prefix that will be used to filter the files that contain it.
 
-        Parameters:
-        prefix -- prefix to be searched for in the file, it must be a string.
+        Return
+        ------
+        list
+            All files containing the prefix.
 
         """
         regex = rf"^{prefix}(...|\w+)\B.+"  # regular expression
--- tests/test_riordinato.py
+++ tests/test_riordinato.py
@@ -2,32 +2,112 @@ from riordinato.files import Organize
 import pytest
 
 
-@pytest.fixture
-def files():
-    newFiles = ["pythonTutorial.py", "mathExercise.txt", "pythonCourse.txt",
-                "pythonPro.docx", "Python_is_cool.py", "mathProblems.txt",
-                "ScinceU.docx", "ThisIsSpam.xd", "mathForPython.pdf",
-                "MoreSpam.toml", "scinceForComputing.epup", "asdkfñlk.idk"]
+def create(tmp_path, files, dirs):
+    """Create files and directories in a temporal path"""
+    # Create files
+    for file in files:
+        new_file = tmp_path / file
+        new_file.touch()
 
-    instance = Organize("", ".")
-    instance.files = newFiles
-    return instance
+    # Create directories
+    for dir in dirs:
+        new_dir = tmp_path / dir
+        new_dir.mkdir()
 
 
-@pytest.mark.parametrize(
+def get_tmp_files(tmp_path):
+    """Get the files from a folder in the temporal path
+
+    Returns only the file name
+    """
+    # filter files from tmp_path avoiding directories
+    tmp_files = list(filter(lambda x: x.is_file(),
+                            tmp_path.iterdir()))
+    # Create a list of file names
+    tmp_files = [file.name for file in tmp_files]
+
+    return tmp_files
+
+
+test_data = pytest.mark.parametrize(
     "prefix, expected",
     [
         ("python",
-         ['pythonTutorial.py', 'pythonCourse.txt',
-          'pythonPro.docx', 'Python_is_cool.py']),
+         ['Python_is_cool.py', 'pythonTutorial.py',
+          'pythonCourse.txt', 'pythonPro.docx', ]),
 
         ("math",
-         ['mathExercise.txt', 'mathProblems.txt',
-          'mathForPython.pdf']),
+         ['mathForPython.pdf', 'mathProblems.txt',
+          'mathExercise.txt', ]),
 
         ("scince",
-         ['ScinceU.docx', 'scinceForComputing.epup'])
+         ['scinceForComputing.epup', 'ScinceU.docx', ]),
     ]
 )
-def test_getFilesWP(files, prefix, expected):
-    assert files.getFilesWP(prefix) == expected
+
+
+@pytest.fixture
+def instance(tmp_path):
+    files = ["pythonTutorial.py", "mathExercise.txt", "pythonCourse.txt",
+             "pythonPro.docx", "Python_is_cool.py", "mathProblems.txt",
+             "ScinceU.docx", "ThisIsSpam.xd", "mathForPython.pdf",
+             "MoreSpam.toml", "scinceForComputing.epup", "asdkfñlk.idk", ]
+
+    dirs = ["python", "scince", "math"]
+
+    create(tmp_path, files, dirs)
+
+    prefixes = [('python', tmp_path / 'python'),
+                ('scince', tmp_path / 'scince'),
+                ('math', tmp_path / 'math')]
+    instance = Organize(prefixes, tmp_path)
+    return instance
+
+
+@pytest.fixture
+def empty_instance(tmp_path):
+    """An instance of the class without files"""
+    create(tmp_path, [], [])
+    empty = Organize([], tmp_path)
+    return empty
+
+
+@test_data
+def test_getFilesWP(instance, prefix, expected):
+    assert instance.getFilesWP(prefix) == expected
+
+
+def test_empty_getFilesWP(empty_instance):
+    assert empty_instance.getFilesWP('prefix') == []
+
+
+def test_getFiles(tmp_path, instance):
+    expected_files = get_tmp_files(tmp_path)
+
+    assert instance.files == expected_files
+
+
+def test_empty_getFiles(empty_instance):
+    assert empty_instance.getFiles() == []
+
+
+@test_data
+def test_all_moveFiles(tmp_path, instance, prefix, expected):
+    instance.moveFiles()
+    files = get_tmp_files(tmp_path / prefix)
+
+    assert files == expected
+
+@test_data
+def test_specific_moveFiles(tmp_path, instance, prefix, expected):
+    instance.moveFiles(specific=prefix)
+    files = get_tmp_files(tmp_path / prefix)
+    
+    assert files == expected
+
+@test_data
+def test_moveSpecificFiles(tmp_path, instance, prefix, expected):
+    instance.moveSpecificFiles(prefix, tmp_path / prefix)
+    files = get_tmp_files(tmp_path / prefix)
+    
+    assert files == expected
