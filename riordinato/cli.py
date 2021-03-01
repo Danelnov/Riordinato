77114c4491e98aa8a051dc8dddfd584bdd17c186
Dan- <misternutel@gmail.com>
2021-03-01 02:38:57 -0500

    basic command to move files with prefix, created

--- riordinato/cli.py
+++ riordinato/cli.py
@@ -1,11 +1,20 @@
 """Cli app for riordinato"""
+from pathlib import Path
+from riordinato import Riordinato
 import typer
 
 app = typer.Typer()
 
 @app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
+def move(prefix: str, destination: Path):
+    instance = Riordinato(Path.cwd())
+    instance.prefixes[prefix] = destination
+    instance.movefiles(specific=prefix)
+    typer.echo(f"files with prefix {prefix} were moved to destination {destination}")
+
+@app.command()
+def current_dir():
+    typer.echo(Path.cwd())
 
 def main():
     app()
