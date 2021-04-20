b974ab1770fed9fd6e8135113adc9822ad2b01d1
Dan- <misternutel@gmail.com>
2021-04-19 22:24:30 -0500

    The add command now has the exclude and specific options

--- riordinato/cli.py
+++ riordinato/cli.py
@@ -2,9 +2,12 @@
 from pathlib import Path
 from riordinato import Riordinato
 from .cli_utils import get_config_file, get_data
-from typing import List
+ 
 import json
 import typer
+
+from typing import List
+from typing import Optional
 
 
 app = typer.Typer()
@@ -12,15 +15,19 @@ app = typer.Typer()
 
 @app.command()
 def move(
-    ignore: bool = False,
+    ignore: bool = False, 
+    specific: Optional[List[str]] = None,
+    exclude: Optional[List[str]] = None,
 ):
+    """Command to move files containing some prefix"""
     file = get_config_file(ignore)
     riordinato = Riordinato(Path.cwd())
     data = get_data(file)
+    # Add prefixes to a Riordinato object
     for prefix, destination in data.items():
         riordinato.prefixes[prefix] = destination
 
-    riordinato.movefiles()
+    riordinato.movefiles(specific=specific, ignore=exclude)
 
 
 @app.command(name='add')
@@ -29,9 +36,11 @@ def add_prefix(
     destination: Path = typer.Argument(..., exists=True),
     ignore: bool = False,
 ):
+    """Add a new prefix to the json file"""
     file = get_config_file(ignore)
     data = get_data(file)
     with open(file, 'w+') as jfile:
+        # Add a prefix with an absolute path
         data[prefix] = str(destination.absolute())
         jfile.write(json.dumps(data, ensure_ascii=False, indent=4))
 
@@ -41,10 +50,13 @@ def remove_prefix(
     prefixes: List[str],
     ignore: bool = False,
 ):
+    """Remove prefixes"""
     file = get_config_file(ignore)
     data = get_data(file)
+
     with open(file, 'w+') as jfile:
-        if prefixes[0] == '.':
+        # Remove the prefixes
+        if prefixes[0] == '.':  # "." to remove all prefixes
             data = {}
         else:
             for prefix in prefixes:
