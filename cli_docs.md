88a438f91b07a581ac409969284bf4cae0b2dbb9
Dan- <misternutel@gmail.com>
2021-04-28 00:19:19 -0500

    the organize command detects when a directory exists and warns it. The cli documentation was generated

---
+++ cli_docs.md
@@ -0,0 +1,92 @@
+# `riordinato`
+
+Organize your files by prefixes.
+
+**Usage**:
+
+```console
+$ riordinato [OPTIONS] COMMAND [ARGS]...
+```
+
+**Options**:
+
+* `--install-completion`: Install completion for the current shell.
+* `--show-completion`: Show completion for the current shell, to copy it or customize the installation.
+* `--help`: Show this message and exit.
+
+**Commands**:
+
+* `add`: Add a new prefix to the json file.
+* `init`: Create prefixes.json file in current...
+* `organize`: Organize files that have prefixes.
+* `remove`: Remove prefixes.
+
+## `riordinato add`
+
+Add a new prefix to the json file.
+
+**Usage**:
+
+```console
+$ riordinato add [OPTIONS] PREFIX DESTINATION
+```
+
+**Arguments**:
+
+* `PREFIX`: The prefix that the file names should have.  [required]
+* `DESTINATION`: The directory where the files with the prefix will be moved.  [required]
+
+**Options**:
+
+* `--ignore / --no-ignore`: Ignore the prefixes.json file inside the directory.
+* `--help`: Show this message and exit.
+
+## `riordinato init`
+
+Create prefixes.json file in current directory.
+
+**Usage**:
+
+```console
+$ riordinato init [OPTIONS]
+```
+
+**Options**:
+
+* `--help`: Show this message and exit.
+
+## `riordinato organize`
+
+Organize files that have prefixes.
+
+**Usage**:
+
+```console
+$ riordinato organize [OPTIONS]
+```
+
+**Options**:
+
+* `--ignore / --no-ignore`: Ignore the prefixes.json file inside the directory.
+* `--specific TEXT`: Only move files containing these prefixes.
+* `--exclude TEXT`: Ignore all files with these prefixes.
+* `--help`: Show this message and exit.
+
+## `riordinato remove`
+
+Remove prefixes.
+
+**Usage**:
+
+```console
+$ riordinato remove [OPTIONS] PREFIXES...
+```
+
+**Arguments**:
+
+* `PREFIXES...`: The prefixes to be removed from the database.  [required]
+
+**Options**:
+
+* `--ignore / --no-ignore`: Ignore the prefixes.json file inside the directory.
+* `--help`: Show this message and exit.
--- riordinato/cli.py
+++ riordinato/cli.py
@@ -13,13 +13,13 @@ from typing import List
 from typing import Optional
 
 
-app = typer.Typer()
+app = typer.Typer(help="Organize your files by prefixes.")
 
 
 @app.command()
 def init():
     """
-    Create prefixes.json file in current directory
+    Create prefixes.json file in current directory.
     """
     file = Path.cwd() / "prefixes.json"
     if file.exists():
@@ -33,58 +33,80 @@ def init():
 @app.command()
 def organize(
     ignore: bool = typer.Option(
-        False, help="Ignore the prefixes.json file inside the directory", show_default=False),
+        False,
+        show_default=False,
+        help="Ignore the prefixes.json file inside the directory."
+    ),
     specific: Optional[List[str]] = typer.Option(
-        None, help="Only move files containing these prefixes"),
+        None,
+        help="Only move files containing these prefixes."
+    ),
     exclude: Optional[List[str]] = typer.Option(
-        None, help="Ignore all files with these prefixes"),
+        None,
+        help="Ignore all files with these prefixes."
+    ),
 ):
     """
-    Organize files that have prefixes
+    Organize files that have prefixes.
     """
-    file = get_prefixes_file(ignore)
-    data = get_data(file)
-    riordinato = Riordinato(Path.cwd())
+    try:
+        file = get_prefixes_file(ignore)
+        data = get_data(file)
+        riordinato = Riordinato(Path.cwd())
 
-    # get the amount of files in current directory
-    # to later calculate the number of files that were organized
-    get_amount_files = lambda: len([f for f in Path.cwd().iterdir() if f.is_file()])
-    amount_files_before = get_amount_files()
-    
-    # Add prefixes to a Riordinato object
-    for prefix, destination in data.items():
-        riordinato.prefixes[prefix] = destination
-    riordinato.movefiles(specific=specific, ignore=exclude)
+        # get the amount of files in current directory
+        # to later calculate the number of files that were organized
+        def get_amount_files(): return len(
+            [f for f in Path.cwd().iterdir() if f.is_file()])
+        amount_files_before = get_amount_files()
 
-    # calculate files that were organized
-    amount_files_after = get_amount_files()
-    amount_files = amount_files_before - amount_files_after
-    typer.secho(f"{amount_files} files were organized", fg=typer.colors.GREEN)
+        # Add prefixes to a Riordinato object
+        for prefix, destination in data.items():
+            riordinato.prefixes[prefix] = destination
+        riordinato.movefiles(specific=specific, ignore=exclude)
+
+        # calculate files that were organized
+        amount_files_after = get_amount_files()
+        amount_files = amount_files_before - amount_files_after
+        typer.echo(f"{amount_files} files were organized")
+    except FileNotFoundError:
+        typer.echo(f"The directory '{destination}' does not exist.", err=True)
+        typer.echo("If you want to delete it put the following command:")
+        typer.secho(f"riordinato remove {prefix}", bold=True)
 
 
 @app.command(name='add')
 def add_prefix(
     prefix: str = typer.Argument(
-        ..., help="The prefix that the file names should have"),
+        ...,
+        help="The prefix that the file names should have."
+    ),
     destination: Path = typer.Argument(
-        ..., help="The directory where the files with the prefix will be moved", exists=True),
+        ...,
+        exists=True,
+        help="The directory where the files with the prefix will be moved."
+    ),
     ignore: bool = typer.Option(
-        False, help="Ignore the prefixes.json file inside the directory", show_default=False),
+        False,
+        show_default=False,
+        help="Ignore the prefixes.json file inside the directory."
+    ),
 ):
     """
-    Add a new prefix to the json file
+    Add a new prefix to the json file.
     """
     try:
         file = get_prefixes_file(ignore)
         data = get_data(file)
 
         with open(file, 'w+') as jfile:
-            Prefix()[prefix] = destination # check that the prefix is valid
-            data[prefix] = str(destination.absolute()) # Add a prefix with an absolute path
+            Prefix()[prefix] = destination  # check that the prefix is valid
+            # Add a prefix with an absolute path
+            data[prefix] = str(destination.absolute())
             # update the prefixes.json files
             jfile.write(json.dumps(data, ensure_ascii=False, indent=4))
 
-        typer.secho(f"{prefix}:{destination} was added.", fg=typer.colors.GREEN)
+        typer.echo(f"{prefix}:{destination} was added.")
     except InvalidPrefixError:
         typer.echo(f"{prefix} is an invalid prefix", err=True)
 
@@ -92,12 +114,17 @@ def add_prefix(
 @app.command(name='remove')
 def remove_prefix(
     prefixes: List[str] = typer.Argument(
-        ..., help="The prefixes to be removed from the database"),
+        ...,
+        help="The prefixes to be removed from the database."
+    ),
     ignore: bool = typer.Option(
-        False, help="Ignore the prefixes.json file inside the directory", show_default=False),
+        False,
+        show_default=False,
+        help="Ignore the prefixes.json file inside the directory."
+    ),
 ):
     """
-    Remove prefixes
+    Remove prefixes.
     """
     file = get_prefixes_file(ignore)
     data = get_data(file)
